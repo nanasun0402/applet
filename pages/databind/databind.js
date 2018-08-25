3b44241fb0f9081ed0478e9858306729ae4b19f2
nana402 <1244481529@qq.com>
2018-08-25 23:19:44 +0800

    wechat applet first commit

---
+++ pages/databind/databind.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    content: '微信小程序数据绑定内容',
+    hiddencontent: "隐藏的内容",
+    flag: false,
+    num1: 1,
+    num2: 2,
+    user: {
+      name: "spark",
+      age: 18
+    }
+  }
+})
